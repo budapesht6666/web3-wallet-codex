7d836fecc9870b5f4458b513e8b7aed6c0ce2be3
budapesht666 <budapesht666@protonmail.com>
2025-10-25 22:55:09 +0300

    feat: create Arbitrum Goerli transfer app

---
+++ src/components/ui/alert.tsx
@@ -0,0 +1,50 @@
+import * as React from "react";
+
+import { cn } from "@/lib/utils";
+
+const alertVariants = {
+  default:
+    "relative w-full rounded-lg border border-border bg-card px-4 py-3 text-sm text-foreground",
+  destructive:
+    "relative w-full rounded-lg border border-destructive/50 bg-destructive/15 px-4 py-3 text-sm text-destructive"
+};
+
+type AlertVariant = keyof typeof alertVariants;
+
+export interface AlertProps extends React.HTMLAttributes<HTMLDivElement> {
+  variant?: AlertVariant;
+}
+
+const Alert = React.forwardRef<HTMLDivElement, AlertProps>(
+  ({ className, variant = "default", ...props }, ref) => (
+    <div
+      ref={ref}
+      role="alert"
+      className={cn(alertVariants[variant], className)}
+      {...props}
+    />
+  )
+);
+Alert.displayName = "Alert";
+
+const AlertTitle = React.forwardRef<
+  HTMLParagraphElement,
+  React.HTMLAttributes<HTMLHeadingElement>
+>(({ className, ...props }, ref) => (
+  <h5
+    ref={ref}
+    className={cn("mb-1 font-semibold leading-none tracking-tight", className)}
+    {...props}
+  />
+));
+AlertTitle.displayName = "AlertTitle";
+
+const AlertDescription = React.forwardRef<
+  HTMLParagraphElement,
+  React.HTMLAttributes<HTMLParagraphElement>
+>(({ className, ...props }, ref) => (
+  <div ref={ref} className={cn("text-sm", className)} {...props} />
+));
+AlertDescription.displayName = "AlertDescription";
+
+export { Alert, AlertTitle, AlertDescription };
